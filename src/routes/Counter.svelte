ef5f01ce9e686320333eec9eb35fc77cec4a9785
dyatohan <d.tohan@work-with-nerds.ca>
2023-06-05 15:26:42 +0300

    updates

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,39 +1,54 @@
 <script>
 	import { spring } from 'svelte/motion';
 
-	let count[] = ;
+	// let count[] = ;
 
-	const displayed_count = spring();
-	$: displayed_count.set(
-  
+	// const displayed_count = spring();
+	// $: displayed_count.set();
+	// $: offset = modulo($displayed_count, 1);
 
-<div class="image-container">
-    <img src="shahd-1.gif" alt="Animation">
-    <br>
-    <img src="img.jpg" alt="Animation">
-    <!--    <br>-->
-</div>
-<div class="image-container2">
-    <img src="image.jpg" alt="Image">
-    <br>
-    <img src="21st-birthday-9.gif" alt="Animation">
+	// /**
+	//  * @param {number} n
+	//  * @param {number} m
+	//  */
+	// function modulo(n, m) {
+	// 	// handle negative numbers
+	// 	return ((n % m) + m) % m;
+	// }
 
-</div>
-);
-	$: offset = modulo($displayed_count, 1);
+	var imageUrls = [
+        "static/img.jpg",
+        "static/shahd-1.gif",
+        "static/image.jpg",
+        "static/21st-birthday-9.gif"
+    ];
 
-	/**
-	 * @param {number} n
-	 * @param {number} m
-	 */
-	function modulo(n, m) {
-		// handle negative numbers
-		return ((n % m) + m) % m;
-	}
+    // Get the button and image elements
+    var counterButton = document.getElementById('counter-button');
+    var imageElement = document.getElementById('image');
+
+    // Counter to keep track of the current image index
+    var counter = 0;
+
+    // Function to update the image source
+    function updateImage() {
+        imageElement.src = imageUrls[counter];
+    }
+
+    // Initial image update
+    updateImage();
+
+    // Event listener for the counter button
+    counterButton.addEventListener('click', function () {
+        // Increment the counter and wrap around if it exceeds the array length
+        counter = (counter + 1) % imageUrls.length;
+        // Update the image
+        updateImage();
+    });
 </script>
 
-<div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+<div class="image-container">
+	<button  id="counter-button" on:click={() => (counterButton)} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -41,12 +56,12 @@
 
 	<div class="counter-viewport">
 		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-			<strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
-			<strong>{Math.floor($displayed_count)}</strong>
+			<strong class="hidden" aria-hidden="true">{<img id="image" src="" alt="Image">}</strong>
+			<!-- <strong>{Math.floor($displayed_count)}</strong> -->
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button  id="counter-button" on:click={() => (counterButton)} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
@@ -54,14 +69,19 @@
 </div>
 
 <style>
-	.counter {
+
+	/* Add CSS styles for image layout */
+	.image-container {
 		display: flex;
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
+		flex-wrap: wrap;
+            justify-content: center;
+            gap: 20px;
 	}
 
-	.counter button {
+	.counter-button {
 		width: 2em;
 		padding: 0;
 		display: flex;
@@ -72,8 +92,12 @@
 		touch-action: manipulation;
 		font-size: 2rem;
 	}
+	.image-container img {
+            max-width: 100%;
+            height: auto;
+        }
 
-	.counter button:hover {
+	.counter-button:hover {
 		background-color: var(--color-bg-1);
 	}
 
@@ -118,36 +142,4 @@
 		top: -100%;
 		user-select: none;
 	}
-
-	
-	img {
-            width: 300px;
-            height: auto;
-        }
-
-        /* Add CSS styles for image layout */
-        .image-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            gap: 20px;
-        }
-
-        .image-container img {
-            max-width: 100%;
-            height: auto;
-        }
-
-
-        .image-container2 {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            gap: 20px;
-        }
-
-        .image-container2 img {
-            max-width: 100%;
-            height: auto;
-        }
 </style>
